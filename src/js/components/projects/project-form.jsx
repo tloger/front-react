e1f5dc18b7dac3a643d640953f942f8302050659
“Sumit <er.sumitarora@gmail.com>
2015-02-22 19:05:00 -0500

    adding validations

--- src/js/components/projects/project-form.jsx
+++ src/js/components/projects/project-form.jsx
@@ -1,9 +1,11 @@
 /** @jsx React.DOM */
 var React = require('react');
 var ClientCombo = require('./client-combo.jsx');
+var ReactWidgets = require('react-widgets');
+var DropdownList = ReactWidgets.DropdownList;
+var is = require('is_js');
 
 var ProjectForm = React.createClass({
-
   propTypes: {
     onSave: React.PropTypes.func.isRequired,
     selectedClient: React.PropTypes.object
@@ -16,11 +18,17 @@ var ProjectForm = React.createClass({
     this.setState({project: this.state.project});
   },
   saveClick: function(event) {
-    if(this.state.project.name != '') {
+    if(is.string(this.state.project.name)) {
+      if(this.selectedClient) {
+        this.state.project.clientId = this.selectedClient.id;
+      }
       this.props.onSave(this.state.project);  
     } else {
 
     }
+  },
+  selectChanged: function(val) {
+    this.selectedClient = val;
   },
   cancelClick: function(event) {
     this.setState({project:{name: ''}});
@@ -28,14 +36,17 @@ var ProjectForm = React.createClass({
   render: function() {
     var name = this.state.project.name;
     return (
-      <div className='form-inline'>
-        <input type="text" className="form-control" placeholder='Project Name' value={name} onChange={this.handleChange} />
-        &nbsp;&nbsp;
-        <ClientCombo selectedClient={this.props.selectedClient} clients={this.props.clients} />
-        &nbsp;&nbsp;
-        <button className='btn btn-success btn-sm' onClick={this.saveClick}>Save</button>
-         &nbsp;&nbsp;
-         <button className='btn btn-danger btn-sm' onClick={this.cancelClick}>Cancel</button>        
+      <div className='row'>
+        <div className="col-sm-3 col-sm-offset-4">
+          <input type="text" className="form-control" placeholder='Project Name' value={name} onChange={this.handleChange} />
+        </div>
+        <div className="col-sm-3">
+          <DropdownList onSelect={this.selectChanged} data={this.props.clients} valueField='id' textField='name' />
+        </div>
+        <div className="col-sm-2 pull-right">
+          <button className='btn btn-success btn-sm' onClick={this.saveClick}>Save</button>
+          <button className='btn btn-danger btn-sm' onClick={this.cancelClick}>Cancel</button>
+        </div>
       </div>
     );
   }
